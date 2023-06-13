e8b1272a5d2e4eb0848a037136926c58291ba9fd
sadiawcutecat <98077710+sadiawcutecat@users.noreply.github.com>
2023-06-13 16:23:32 +0600

    readme file upgraded

--- src/Component/Pages/PopularInstructor/PopularInstructor.jsx
+++ src/Component/Pages/PopularInstructor/PopularInstructor.jsx
@@ -4,13 +4,13 @@ import { Link } from "react-router-dom";
 
 
 const PopularInstructor = () => {
-    const [instructors, setinstructor] = useState([]);
+    const [instructors, setInstructor] = useState([]);
 
     useEffect(() => {
         fetch('/public/instructor.json')
             .then(res => res.json())
             .then(data => {
-                setinstructor(data.instructors);
+                setInstructor(data.instructors);
 
             })
             .catch(error => console.log(error))
@@ -21,38 +21,31 @@ const PopularInstructor = () => {
             <h1 className="text-primary text-opacity-70 font-bold font-mono text-4xl text-center my-9">Popular Instructors Section</h1>
 
             <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-            
+                {
+                    instructors?.map(instructor => <>
 
+                        <div>
 
-
-            {
-                instructors?.map(instructor => <>
-
-                    <div>
-
-                        <div className="card w-96 bg-base-100 shadow-xl">
-                            <figure className="px-10 pt-10">
-                                <img src={instructor.image} alt="Shoes" className="rounded-xl h-32" />
-                            </figure>
-                            <div className="card-body items-center text-center font-mono">
-                                <h2 className="card-title text-orange-500 font-mono font-bold">{instructor.name}</h2>
-                                <p>Country: {instructor.country}</p>
-                                <Link className="link-hover "><p>{instructor.email}</p></Link>
-                                <p>classes_taken: {instructor.classes_taken}</p>
-                                <div className="card-actions">
-                                    <button className="btn btn-primary">Buy Now</button>
+                            <div className="card w-96 bg-base-100 shadow-xl">
+                                <figure className="px-10 pt-10">
+                                    <img src={instructor.image} alt="Shoes" className="rounded-xl h-32" />
+                                </figure>
+                                <div className="card-body items-center text-center font-mono">
+                                    <h2 className="card-title text-orange-500 font-mono font-bold">{instructor.name}</h2>
+                                    <p>Country: {instructor.country}</p>
+                                    <Link className="link-hover "><p>{instructor.email}</p></Link>
+                                    <p>classes_taken: {instructor.classes_taken}</p>
+                                    <div className="card-actions">
+                                        <button className="btn btn-primary">Buy Now</button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                </>)
-            }
-
-
-
+                    </>)
+                }
+            </div>
         </div>
-        </div>
-       
+
     );
 };
 
